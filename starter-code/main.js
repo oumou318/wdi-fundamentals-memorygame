dfa225eb7d1cc1a7931a1345a38c9ac5aeb968e9
Oumou <Oumou@oumous-mbp.wifi.lagcc>
2017-03-22 19:28:24 -0400

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,6 @@
 console.log("JS file is connected to HTML! Woo!")
+var cardOne = "Queen";
+var cardTwo = "Queen";
+var cardThree = "King";
+var cardFour = "King";
+// console.log(cardFour); you can type in th console cardFour === cardOne n test it
