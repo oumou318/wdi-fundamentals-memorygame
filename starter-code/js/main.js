140c1267b2373d4ef1fd9980744893ead230a399
Oumou <Oumou@oumous-mbp.wifi.lagcc>
2017-03-24 16:12:28 -0400

    unit11

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -1,8 +1,8 @@
 console.log("JS file is connected to HTML! Woo!")
-//var cardOne = "Queen";
-//var cardTwo = "Queen";
-//var cardThree = "King";
-//var cardFour = "King";
+var cardOne = "Queen";
+var cardTwo = "Queen";
+var cardThree = "King";
+var cardFour = "King";
 // console.log(cardFour); you can type in th console cardFour === cardOne n test it
 //  if(cardOne === cardTwo) ||(cardFour === cardThree){
 // 	window.alert("You found a match!");
@@ -40,7 +40,6 @@ function createCards (){
 		gameBoard.appendChild(cardEl);
 		cardEl.setAttribute('data-card', cards[i]);
 		cardEl.addEventListener('click', isTwoCards);
-		// cardEl.innerHTML = '<img src="images/diamondsKing.png" alt="King of diamonds">'
 	}
 }
 var cards = ['King', 'Queen', 'King', 'Queen'];
@@ -49,35 +48,19 @@ var cardsInPlay = [];
 
 function isTwoCards(){
 	cardsInPlay.push(this.getAttribute('data-card'));
-	//console.log(this.getAttribute('data-card'));
-	 if (this.getAttribute('data-card')==='King') {
-	 	this.innerHTML = '<img src="images/diamondsKing.png" alt="King of diamonds">';
-	} 
-	if (this.getAttribute('data-card')==='Queen') {
-		this.innerHTML = '<img src="images/diamondsQueen.png" alt="Queen of diamonds">';
-	}
 	 if (cardsInPlay.length === 2) {
 	 	 isMatch(cardsInPlay);
 	 	  cardsInPlay = [];
 	}
 }
 
-
-function isMatch() {
-	if (cards[0] === cards[2]) {
-		alert("You found a match!");
-	  } else { 
-	 	alert("Sorry try again!");}
-isMatch(cardsInPlay);
-console.log(cardsInPlay);
-
+function isMatch(cardsInPlay) {
+	 // isMatch(cardsInPlay);
+	 console.log(cardsInPlay);
 }
-//if (this.getAttribute('data-card') === 'Queen') {
-//cardEl.innerHTML = '<img src="images/diamondsQueen.png" alt="Queen of diamonds">'
- 
+
+
 createCards();
-// isMatch();
-// isTwoCards();
 
 
 
